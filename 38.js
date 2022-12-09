fcf2a9c614f345d9cdc4d986a50d4ddb9ded2878
Андрей <Ands2909902@gmail.com>
2022-12-09 20:31:30 +0300

    38

---
+++ 38.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myNoun+" "+myVerb+" "+myAdverb+" q "+myAdjective+" q"; // Change this line
+// Only change code above this line
