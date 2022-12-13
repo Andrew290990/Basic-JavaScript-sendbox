7881a6a771bdbf52f1ae73f9a73309108d98ba00
Андрей <Ands2909902@gmail.com>
2022-12-13 19:51:15 +0300

    83

---
+++ 83.js
@@ -0,0 +1,10 @@
+const myDog = {
+    // Only change code below this line
+    
+    "name": "garik))",
+    "legs": 4,
+    "tails": 2,
+    "friends": ["robin", "batmen"]
+  
+    // Only change code above this line
+  };
